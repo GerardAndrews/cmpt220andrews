69544ee3e7e1ea8153bf5fea927bafdad6ea21e0
Gerard Andrews <gerard.andrews1@marist.edu>
2017-01-22 20:06:49 -0500

    Lab 1 first problem complete

---
+++ cmpt220andrews/labs/1/Lab1Problem1.java
@@ -0,0 +1,16 @@
+//Gerard Andrews 
+//1/22/17 
+//ProgrammingExercise 1.3
+public class Lab1Problem1{
+  public static void main(String[] args) {
+	  System.out.println("    J    A    V     V    A");
+	  System.out.println("    J   A A    V   V    A A");
+	  System.out.println("J   J  AAAAA    V V    AAAAA");
+	  System.out.println(" JJ   A     A    V    A     A");
+  }
+}
+
+    
+    
+    
+    
--- cmpt220andrews/prj/0/HelloWorld.java
+++ cmpt220andrews/prj/0/HelloWorld.java
@@ -1,3 +1,6 @@
+//Gerard Andrews
+//1/19/2017
+//Project 0
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello World!");
