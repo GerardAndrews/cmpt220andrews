253625335cc345fea3c832fa4f1aeef62056815e
Gerard Andrews <gerard.andrews1@marist.edu>
2017-01-23 21:25:19 -0500

    Renamed files to better names.

---
+++ cmpt220andrews/labs/1/Problem2_6.java
@@ -0,0 +1,29 @@
+//Gerard Andrews
+//1/22/2017
+//Lab1, Part 1, Problem 4
+import java.util.Scanner;
+
+public class Problem2_6{
+	public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+	  
+	  System.out.println("Enter a number between 0 and 1000: ");
+	  int inputNum = input.nextInt();
+	  
+	  int digitOne = inputNum % 10;
+	  int extractOne = inputNum / 10;
+	  
+	  int digitTwo = extractOne % 10;
+	  int extractTwo = extractOne / 10;
+	  
+	  int digitThree = extractTwo % 10;
+	  int extractThree = extractTwo / 10;
+	  
+	  int digitFour = extractThree % 10;
+	  
+	  int sum = digitOne + digitTwo + digitThree + digitFour;
+	  
+	  System.out.println("The sum of the digits is: " + sum + ".");
+
+	}
+}
