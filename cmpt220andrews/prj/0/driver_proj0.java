b4caa281dceec7d9082b4055f60b8d605f02cc77
Gerard Andrews <gerard.andrews1@marist.edu>
2017-01-23 21:05:27 -0500

    added screenshot to prove I have passed Kattis judgement for proj 0

--- cmpt220andrews/prj/0/driver_proj0.java
+++ cmpt220andrews/prj/0/driver_proj0.java
@@ -1,7 +1,7 @@
 //Gerard Andrews
 //1/19/2017
 //Project 0
-public class HelloWorld {
+public class driver_proj0 {
   public static void main(String[] args) {
     System.out.println("Hello World!");
   }
