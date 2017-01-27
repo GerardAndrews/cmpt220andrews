f8cfa2fc5aee1aa14b42b239655214e74ce50a1c
Gerard Andrews <gerard.andrews1@marist.edu>
2017-01-27 13:26:02 -0500

    fixing spacing issues with scanner input lines.

--- cmpt220andrews/labs/1/LabStep_2.java
+++ cmpt220andrews/labs/1/LabStep_2.java
@@ -12,7 +12,7 @@ import java.util.Scanner;
 
 public class LabStep_2{
 	public static void main(String[] args) {
-      Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 	  
 	  System.out.println("Please enter the following as a percentage: ");
 	  System.out.println("Midterm Grade: ");
