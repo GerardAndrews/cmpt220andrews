7df5e9a22775e6215b931ac77460ff5f9783b933
Gerard Andrews <gerard.andrews1@marist.edu>
2017-01-27 20:12:46 -0500

    Update Problem2_5.java

--- cmpt220andrews/labs/1/Problem2_5.java
+++ cmpt220andrews/labs/1/Problem2_5.java
@@ -12,15 +12,15 @@ import java.util.Scanner;
 public class Problem2_5 {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
-	System.out.println("Enter the subtotal: ");
-	double subtotal = input.nextDouble();
-	System.out.println("Enter the gratuity rate: ");
-	double gratuityrate = input.nextDouble();
-	//Multiplying the subtotal and gratuity will give you the gratuity.
-	double gratuity = subtotal * gratuityrate / 100;
-	//Adding subtotal and the gratuity then gives you the total
-	double total = subtotal + gratuity;
-	System.out.println("The gratuity is" + gratuity + 
-	" and the total is " + total + " .");
+    System.out.println("Enter the subtotal: ");
+    double subtotal = input.nextDouble();
+    System.out.println("Enter the gratuity rate: ");
+    double gratuityrate = input.nextDouble();
+    //Multiplying the subtotal and gratuity will give you the gratuity.
+    double gratuity = subtotal * gratuityrate / 100;
+    //Adding subtotal and the gratuity then gives you the total
+    double total = subtotal + gratuity;
+    System.out.println("The gratuity is" + gratuity + 
+    " and the total is " + total + " .");
   }
 }
