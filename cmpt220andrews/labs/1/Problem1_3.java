1133e98698a543595f4d6d5df1c2a4a39a212a1a
Gerard Andrews <gerard.andrews1@marist.edu>
2017-01-27 19:27:41 -0500

    Parts 1-2 of lab 1 meet coding style.

--- cmpt220andrews/labs/1/Problem1_3.java
+++ cmpt220andrews/labs/1/Problem1_3.java
@@ -7,13 +7,13 @@ Due Date: 1/24/2017
 Version 1.0
 /This file outputs: "JAVA" in string
 */
-public class Problem1_3{
-    public static void main(String[] args) {
+public class Problem1_3 {
+  public static void main(String[] args) {
 	  
-      System.out.println("    J    A    V     V    A");
-      System.out.println("    J   A A    V   V    A A");
-      System.out.println("J   J  AAAAA    V V    AAAAA");
-      System.out.println(" JJ   A     A    V    A     A");
+    System.out.println("    J    A    V     V    A");
+    System.out.println("    J   A A    V   V    A A");
+    System.out.println("J   J  AAAAA    V V    AAAAA");
+    System.out.println(" JJ   A     A    V    A     A");
   }
 }
 
--- cmpt220andrews/labs/1/Problem2_6.java
+++ cmpt220andrews/labs/1/Problem2_6.java
@@ -10,28 +10,19 @@ Version 1.0
 import java.util.Scanner;
 
 public class Problem2_6 {
-	public static void main(String[] args) {
-		
-      Scanner input = new Scanner(System.in);
-	  
-	  System.out.println("Enter a number between 0 and 1000: ");
-	  int integer = input.nextInt();
-	  
-	  int digitOne = integer % 10;  //checks the first contant for remainders
-	  int extractOne = integer / 10;  //Divides the digit by 10 to extract the remainder
-	  
-	  int digitTwo = extractOne % 10;  //checks the second contant for remainders
-	  int extractTwo = extractOne / 10;
-	  
-	  int digitThree = extractTwo % 10;  //checks the third contant for remainders
-	  int extractThree = extractTwo / 10;
-	  
-	  int digitFour = extractThree % 10;  //checks the first contant for remainders no extract.
-	  
-	  int sum = digitOne + digitTwo + digitThree + digitFour; 
-	  //computes the sum of all extracted digits
-	  
-	  System.out.println("The sum of the digits is: " + sum + ".");
-
-	}
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+	System.out.println("Enter a number between 0 and 1000: ");
+	int integer = input.nextInt();
+	int digitOne = integer % 10;  //checks the first contant for remainders
+	int extractOne = integer / 10;  //Divides the digit by 10 to extract the remainder
+	int digitTwo = extractOne % 10;  //checks the second contant for remainders
+	int extractTwo = extractOne / 10;
+	int digitThree = extractTwo % 10;  //checks the third contant for remainders
+	int extractThree = extractTwo / 10;
+	int digitFour = extractThree % 10;  //checks the first contant for remainders no extract.
+	int sum = digitOne + digitTwo + digitThree + digitFour; 
+	//computes the sum of all extracted digits
+	System.out.println("The sum of the digits is: " + sum + ".");
+  }
 }
