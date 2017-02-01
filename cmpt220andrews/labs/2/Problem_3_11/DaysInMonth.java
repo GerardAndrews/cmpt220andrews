9cc5208c22a069b9330a226541c4e2d63f0340a7
Gerard Andrews <gerard.andrews1@marist.edu>
2017-02-01 17:36:15 -0500

    Fixed Spacing

--- cmpt220andrews/labs/2/Problem_3_11/DaysInMonth.java
+++ cmpt220andrews/labs/2/Problem_3_11/DaysInMonth.java
@@ -13,39 +13,39 @@ import java.util.Scanner;
 public class DaysInMonth {
   public static void main(String[] args){
     Scanner input = new Scanner(System.in);
-	System.out.println("Enter the month as a number between 1 and 12: ");
-	int month = input.nextInt();
-	System.out.println("Enter a year number: ");
-	int year = input.nextInt();
-	//Leapyear is a factor here for determining February specifically,
-	//So it's important to determine if it's true or not.
-	boolean isLeapYear = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
-	switch (month) {
-	  case 1: System.out.println("January, " + year + " has/had 31 days"); 
-	  break;
-	  case 2: System.out.println("February, " + year + " has/had" + 
-	  ((isLeapYear) ?  " 29 days" : "28 days")); 
-	  break;
-	  case 3: System.out.println("March, " + year + " has/had 31 days"); 
-	  break;
-	  case 4: System.out.println("April, " + year + " has/had 30 days");
-	  break;
-	  case 5: System.out.println("May, " + year + " has/had 31 days");
-	  break;
-	  case 6: System.out.println("June, " + year + " has/had 30 days");
-	  break;
-	  case 7: System.out.println("July, " + year + " has/had 31 days");
-	  break;
-	  case 8: System.out.println("August, " + year + " has/had days");
-	  break;
-	  case 9: System.out.println("September, " + year + " has/had 30 days");
-	  break;
-	  case 10: System.out.println("October, " + year + " has/had 31 days");
-	  break;
-	  case 11: System.out.println("November, " + year + " has/had 30 days");
-	  break;
-	  case 12: System.out.println("December, " + year + " has/had 31 days");
-	  break;
+    System.out.println("Enter the month as a number between 1 and 12: ");
+    int month = input.nextInt();
+    System.out.println("Enter a year number: ");
+    int year = input.nextInt();
+    //Leapyear is a factor here for determining February specifically,
+    //So it's important to determine if it's true or not.
+    boolean isLeapYear = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    switch (month) {
+      case 1: System.out.println("January, " + year + " has/had 31 days"); 
+      break;
+      case 2: System.out.println("February, " + year + " has/had" + 
+      ((isLeapYear) ?  " 29 days" : "28 days")); 
+        break;
+      case 3: System.out.println("March, " + year + " has/had 31 days"); 
+        break;
+      case 4: System.out.println("April, " + year + " has/had 30 days");
+        break;
+      case 5: System.out.println("May, " + year + " has/had 31 days");
+        break;
+      case 6: System.out.println("June, " + year + " has/had 30 days");
+        break;
+      case 7: System.out.println("July, " + year + " has/had 31 days"); 
+        break;
+      case 8: System.out.println("August, " + year + " has/had days");
+        break;
+      case 9: System.out.println("September, " + year + " has/had 30 days");   	 
+	break;
+      case 10: System.out.println("October, " + year + " has/had 31 days");
+	break;
+      case 11: System.out.println("November, " + year + " has/had 30 days");
+        break;
+      case 12: System.out.println("December, " + year + " has/had 31 days");
+        break;
     }
   }
 }
