fa17f978b714f14753f6a21cdb71c15b9bcdee19
Gerard Andrews <gerard.andrews1@marist.edu>
2017-02-01 19:18:27 -0500

    Fixed Spacing

--- cmpt220andrews/labs/2/Problem_4_5/PolygonArea.java
+++ cmpt220andrews/labs/2/Problem_4_5/PolygonArea.java
@@ -13,11 +13,11 @@ import java.util.Scanner;
 public class PolygonArea {
   public static void main(String[] args){
     Scanner input = new Scanner(System.in);
-	System.out.println("Enter the number of sides: ");
-	double n = input.nextDouble();
-	System.out.println("Enter the side length: ");
-	double s = input.nextDouble();
-	double area = (n * Math.pow(s, 2)) / (4 * Math.tan(Math.PI / n));
-	System.out.println("The area of the polygon is: " + area);
+    System.out.println("Enter the number of sides: ");
+    double n = input.nextDouble();
+    System.out.println("Enter the side length: ");   
+    double s = input.nextDouble();
+    double area = (n * Math.pow(s, 2)) / (4 * Math.tan(Math.PI / n));
+    System.out.println("The area of the polygon is: " + area);
   }
 }
