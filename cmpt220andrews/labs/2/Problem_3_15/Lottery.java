0caece49188a1eac683bf7f53dbbee260cde5975
Gerard Andrews <gerard.andrews1@marist.edu>
2017-02-07 16:24:41 -0500

    Fixed Spacing

--- cmpt220andrews/labs/2/Problem_3_15/Lottery.java
+++ cmpt220andrews/labs/2/Problem_3_15/Lottery.java
@@ -38,11 +38,11 @@ public class Lottery {
       case 1: System.out.println("Exact match: you win $10,000.");
         break;
       case 2: System.out.println("Match all digits: you win $3,000.");	  
-	    break;
+	break;
       case 3: System.out.println("Match one digit: you win $1,000.");
         break;
       case 4: System.out.println("Sorry, no match.");
-	    break;
+	break;
     }
   }
 }
