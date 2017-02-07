d6e1fbb44a421793d86a942c420ee69465e0fd22
Gerard Andrews <gerard.andrews1@marist.edu>
2017-02-07 16:18:09 -0500

    Fixed Spacing

--- cmpt220andrews/labs/2/Part2/GradeCalculation.java
+++ cmpt220andrews/labs/2/Part2/GradeCalculation.java
@@ -13,7 +13,7 @@ import java.util.Scanner;
 public class GradeCalculation {
   public static void main(String[] args){
     Scanner input = new Scanner(System.in);
-	System.out.println("Please enter the following as a percentage: ");
+    System.out.println("Please enter the following as a percentage: ");
     System.out.println("Midterm Grade: ");
     //Takes the midterm grade input and divides with percentage weight of our sd1 grade rubric
     double midterm = input.nextDouble() * 0.2;  
@@ -27,29 +27,28 @@ public class GradeCalculation {
     //Takes the hw and labs grade input and divides with percentage weight of our sd1 grade rubric
     double hwLabs = input.nextDouble() * 0.4;
     double finalGrade = (midterm + finalTest + projects + hwLabs);
-	System.out.println("Percentage Grade is " + finalGrade + ".");
-	if (finalGrade >= 95.0) 
-		System.out.println("Your letter Grade is an A");
-	else if (finalGrade >= 90.0) 
-		System.out.println("Your letter Grade is an A-");
-	else if (finalGrade >= 87.0) 
-		System.out.println("Your letter Grade is a B+");
-	else if (finalGrade >= 83.0) 
-		System.out.println("Your letter Grade is a B");
-	else if (finalGrade >= 80.0) 
-		System.out.println("Your letter Grade is a B-");
-	else if (finalGrade >= 77.0)
-		System.out.println("Your letter Grade is a C+");
-	else if (finalGrade >= 73.0) 
-		System.out.println("Your letter Grade is a C");
-	else if (finalGrade >= 70.0) 
-		System.out.println("Your letter Grade is a C-");
-	else if (finalGrade >= 65.0) 
-		System.out.println("Your letter Grade is a D+");
-	else if (finalGrade >= 60.0) 
-		System.out.println("Your letter Grade is a D");
-	else 
-		System.out.println("Your letter Grade is an F");
-	
+    System.out.println("Percentage Grade is " + finalGrade + ".");
+    if (finalGrade >= 95.0) 
+      System.out.println("Your letter Grade is an A");
+    else if (finalGrade >= 90.0) 
+      System.out.println("Your letter Grade is an A-");
+    else if (finalGrade >= 87.0) 
+      System.out.println("Your letter Grade is a B+");
+    else if (finalGrade >= 83.0) 
+      System.out.println("Your letter Grade is a B");
+    else if (finalGrade >= 80.0) 
+      System.out.println("Your letter Grade is a B-");
+    else if (finalGrade >= 77.0)
+      System.out.println("Your letter Grade is a C+");
+    else if (finalGrade >= 73.0) 
+      System.out.println("Your letter Grade is a C");
+    else if (finalGrade >= 70.0) 
+      System.out.println("Your letter Grade is a C-");
+    else if (finalGrade >= 65.0) 
+      System.out.println("Your letter Grade is a D+");
+    else if (finalGrade >= 60.0) 
+      System.out.println("Your letter Grade is a D");
+    else 
+      System.out.println("Your letter Grade is an F");
   }
 }
