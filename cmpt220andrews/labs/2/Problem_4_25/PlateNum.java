b70851d01212a0449aa2aff7512a9ed7664f4794
Gerard Andrews <gerard.andrews1@marist.edu>
2017-02-13 14:11:52 -0500

    Fixed Spacing on Problem 4_25

--- cmpt220andrews/labs/2/Problem_4_25/PlateNum.java
+++ cmpt220andrews/labs/2/Problem_4_25/PlateNum.java
@@ -12,16 +12,16 @@ followed by 4 digits.
 public class PlateNum {
   public static void main(String[] args){
     //Generate three random uppercase letters
-	int letter1 = 65 + (int)(Math.random() * (90 - 65));
-	int letter2 = 65 + (int)(Math.random() * (90 - 65));
-	int letter3 = 65 + (int)(Math.random() * (90 - 65));	
-	//Generate four random digits
-	int num1 = (int)(Math.random() * 10);
-	int num2 = (int)(Math.random() * 10);
-	int num3 = (int)(Math.random() * 10);
-	int num4 = (int)(Math.random() * 10);
-	//Display number plate
-	System.out.println("" + (char)(letter1) + ((char)(letter2)) + 
-	((char)(letter3)) + num1 + num2 + num3 + num4);
+    int letter1 = 65 + (int)(Math.random() * (90 - 65));
+    int letter2 = 65 + (int)(Math.random() * (90 - 65));
+    int letter3 = 65 + (int)(Math.random() * (90 - 65));	
+    //Generate four random digits
+    int num1 = (int)(Math.random() * 10);
+    int num2 = (int)(Math.random() * 10);
+    int num3 = (int)(Math.random() * 10);
+    int num4 = (int)(Math.random() * 10);
+    //Display number plate
+    System.out.println("" + (char)(letter1) + ((char)(letter2)) + 
+    ((char)(letter3)) + num1 + num2 + num3 + num4);
   }
 }
