d602d712716744403fb2d659035d4cf2b255f39f
Gerard Andrews <gerard.andrews1@marist.edu>
2017-02-18 16:09:16 -0500

    Fixed Spacing

--- cmpt220andrews/labs/3/Problem5_12/NSquared.java
+++ cmpt220andrews/labs/3/Problem5_12/NSquared.java
@@ -9,13 +9,13 @@ Version 1.0
 */
 public class NSquared {
   public static void main(String[] args){
-	int n = 0;	// Start n at 0
-	//Find the smallest n such that n^2 > 12,000
-	while (Math.pow(n, 2) <= 12000) {
-	  //Increment n
-	  n++;
-	}
-	//Display result
-	System.out.println("The smallest integer n such that n^2 is greater than 12,000: " + n);
+  int n = 0;	// Start n at 0
+  //Find the smallest n such that n^2 > 12,000
+  while (Math.pow(n, 2) <= 12000) {
+    //Increment n
+    n++;
+  }
+  //Display result
+  System.out.println("The smallest integer n such that n^2 is greater than 12,000: " + n);
   }
 }
