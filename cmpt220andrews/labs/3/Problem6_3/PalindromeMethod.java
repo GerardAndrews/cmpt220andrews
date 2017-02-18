203dc4295f8ccf43b9926da2156b182195f1e082
Gerard Andrews <gerard.andrews1@marist.edu>
2017-02-18 17:09:29 -0500

    Fixed Spacing

--- cmpt220andrews/labs/3/Problem6_3/PalindromeMethod.java
+++ cmpt220andrews/labs/3/Problem6_3/PalindromeMethod.java
@@ -14,26 +14,26 @@ public class PalindromeMethod {
   public static void main(String[] args){
     Scanner input = new Scanner(System.in);
     System.out.print("Enter an integer: ");
-	int number = input.nextInt();
-	//Report whether the integer is a palindrome.
-	System.out.println(number + (isPalindrome(number) ? " is " : " is not ") +
-	"a palindrome.");
-	}
-	//Method isPalindrome returns true if number is a palindrome
-	public static boolean isPalindrome(int number) {
-	  return number == reverse(number) ? true : false;
-	}
-	//Method reverse returns the reversal of an integer
-	public static int reverse(int number) {
-	  // Holds reversed number
-	  String reverse = ""; 	
-	  //Convert number to string
-	  String n = number + "";
-	  //Reverse string
-	  for (int i = n.length() - 1; i >= 0; i--) {
-	    reverse += n.charAt(i);
-	}
-	// Return reversed integer
-	return Integer.parseInt(reverse);
-	}
+    int number = input.nextInt();
+   //Report whether the integer is a palindrome.
+   System.out.println(number + (isPalindrome(number) ? " is " : " is not ") +
+   "a palindrome.");
+    }
+    //Method isPalindrome returns true if number is a palindrome
+    public static boolean isPalindrome(int number) {
+      return number == reverse(number) ? true : false;
+    }
+    //Method reverse returns the reversal of an integer
+    public static int reverse(int number) {
+      //Holds reversed number
+      String reverse = ""; 	
+      //Convert number to string
+      String n = number + "";
+    //Reverse string
+      for (int i = n.length() - 1; i >= 0; i--) {
+        reverse += n.charAt(i);
+    }
+    // Return reversed integer
+    return Integer.parseInt(reverse);
+  }
 }
