48c240d19ce8bdbd454649daeb1dba8d7e0ecb62
Gerard Andrews <gerard.andrews1@marist.edu>
2017-02-18 15:46:13 -0500

    Fixed Spacing

--- cmpt220andrews/labs/3/Problem5_7/FinanceForLoop.java
+++ cmpt220andrews/labs/3/Problem5_7/FinanceForLoop.java
@@ -12,22 +12,22 @@ import java.util.Scanner;
 public class FinanceForLoop {
   public static void main(String[] args){
     Scanner input = new Scanner(System.in);
-	int totalCost = 0;
-	int tuition = 10000;
-	int tuitionTenthYear = 0;
-	for (int year = 1; year <= 14; year++) {
-	//Increase tution by 5% every year
-	  tuition += (tuition * 0.05);  
-	if (year > 10)
-	  totalCost += tuition;
-	//Cost of tution in ten years
-	if (year == 10)
-	  tuitionTenthYear = tuition; 
-	}
-	//Display the cost of tution in ten years
-	System.out.println("Tuition in ten years: $" + tuitionTenthYear +".");
-	//Display the cost of four years' worth of tution after tenth year
-	System.out.println("Total cost for four years' worth of tuition" +
-	" after the tenth year: $" + totalCost);
+    int totalCost = 0;
+    int tuition = 10000;
+    int tuitionTenthYear = 0;
+    for (int year = 1; year <= 14; year++) {
+    //Increase tution by 5% every year
+      tuition += (tuition * 0.05);  
+    if (year > 10)
+      totalCost += tuition;
+    //Cost of tution in ten years
+    if (year == 10)
+      tuitionTenthYear = tuition;    	
+    }
+      //Display the cost of tution in ten years
+      System.out.println("Tuition in ten years: $" + tuitionTenthYear +".");
+      //Display the cost of four years' worth of tution after tenth year
+      System.out.println("Total cost for four years' worth of tuition" +
+      " after the tenth year: $" + totalCost);
   }
 }
