24fde8ee2fe95ba74c7f57df62a2ffedd23204d8
Gerard Andrews <gerard.andrews1@marist.edu>
2017-02-18 15:19:42 -0500

    fixed spacing

--- cmpt220andrews/labs/3/Problem5_1/NumCount.java
+++ cmpt220andrews/labs/3/Problem5_1/NumCount.java
@@ -14,36 +14,36 @@ import java.util.Scanner;
 public class NumCount {
   public static void main(String[] args){
     Scanner input = new Scanner(System.in);
-	//We need to set variables for positive, negative ints, and total and average calculations.
-	int positives = 0;
-	int negatives = 0;
-	double total = 0;
-	int count = 0;
-	int num = 0;
-	//Now we need a do statement that executes the input
-	do {
-	  System.out.println("Enter an input, (ends if it is 0): ");
-      num = input.nextInt();
-	  if (num == 0) {
-	    System.out.println("Program will now end, 0 was inputed.");
-	    break);
-	  }
-	  else if (num > 0) {
-	    positives++;
-	  }
-	  else {
-	    negatives++;
-	  }
-	  total += num;
-	  count++;
-	  continue;
-	} while (num != 0); 
-	  //Now we calculate the average. & Display results
-	  double average = total/count;
-	  System.out.println("Number of positives is: " + positives + 
-	  " The number of negatives is: " + negatives +
-	  " The count is: " + count + " And the average is: " + average +".");
-	  }
-	}
+    //We need to set variables for positive, negative ints, and total and average calculations.
+    int positives = 0;
+    int negatives = 0;
+    double total = 0;
+    int count = 0;
+    int num = 0;
+   //Now we need a do statement that executes the input
+    do {
+      System.out.println("Enter an input, (ends if it is 0): ");
+        num = input.nextInt();
+      if (num == 0) {
+	System.out.println("Program will now end, 0 was inputed.");
+	break);
+      }
+      else if (num > 0) {
+       positives++;
+      }
+      else {
+       negatives++;
+      }
+      total += num;
+      count++;
+      continue;
+      } while (num != 0); 
+      //Now we calculate the average. & Display results
+      double average = total/count;
+      System.out.println("Number of positives is: " + positives + 
+     " The number of negatives is: " + negatives +
+     " The count is: " + count + " And the average is: " + average +"."); 
+  }
+}
 	
 	
