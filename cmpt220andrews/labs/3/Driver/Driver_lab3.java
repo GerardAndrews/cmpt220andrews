27daed7116c6e12db05051a75b0b17d4e31a8487
Gerard Andrews <gerard.andrews1@marist.edu>
2017-02-19 18:09:18 -0500

    Fixed Spacing

--- cmpt220andrews/labs/3/Driver/Driver_lab3.java
+++ cmpt220andrews/labs/3/Driver/Driver_lab3.java
@@ -14,15 +14,15 @@ public class Driver_lab3 {
   public static void main(String[] args){
     Scanner scan = new Scanner(System.in);
     while (true) {
-	  double x1 = scan.nextDouble();
-	  if (x1 == 0)
-		break;
-	  double y1 = scan.nextDouble();
-	  double x2 = scan.nextDouble();
-	  double y2 = scan.nextDouble();
-	  double p = scan.nextDouble();
-	  System.out.printf( "%.10f" , Math.pow(Math.pow(Math.abs(x1 - x2) , p) + Math.pow(Math.abs(y1 - y2), p), 1 / p));
-	  System.out.println();
+      double x1 = scan.nextDouble();
+	if (x1 == 0)
+          break;
+      double y1 = scan.nextDouble();
+      double x2 = scan.nextDouble();
+      double y2 = scan.nextDouble();
+      double p = scan.nextDouble();
+      System.out.printf( "%.10f" , Math.pow(Math.pow(Math.abs(x1 - x2) , p) + Math.pow(Math.abs(y1 - y2), p), 1 / p));
+      System.out.println();
     }
     scan.close();
   }
