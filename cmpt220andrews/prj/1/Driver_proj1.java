4279e67e3668cb62bf971541bb63e7b26644ffaf
Gerard Andrews <gerard.andrews1@marist.edu>
2017-03-22 22:05:46 -0400

    Fixed Spacing

--- cmpt220andrews/prj/1/Driver_proj1.java
+++ cmpt220andrews/prj/1/Driver_proj1.java
@@ -1,58 +1,65 @@
+/*
+File: Driver_proj1.java
+Author: Gerard Andrews
+Course: CMPT220
+Assignment: Lab 4
+Due Date: 2/28/2017
+Version 1.0
+/This file prompts the user to input the size and and numbers of two vectors (arrays)
+The vectors then go through the function which convolutes both vectors.
+*/
 import java.util.Scanner;
-import java.io.*;
- 
-public class Driver_proj1 {
 
+public class Driver_proj1 {
   public static void main(String[] args) {
-    //Create a scanner
-    Scanner input = new Scanner(System.in);
+    //Create a scanner  
+    Scanner scanFile = new Scanner(System.in);
     //Create two arrays for representing two vectors, and prompt the user to
     //enter the size of both vectors
     System.out.println("Please enter the size of the first vector: ");
-    int input = input.nextInt();
+    int input = scanFile.nextInt();
     double[] array = new double[input];
     System.out.println("Please enter the size of the 2nd vector: ");
-    input = input.nextInt();
+    input = scanFile.nextInt();
     //Prompt the user to enter the numbers for both vectors
     System.out.println("Please enter the elements of the 1st vector: ");
     for (int vFirst = 0; vFirst < array.length; vFirst++){
-      array[vFirst] = input.nextDouble();
+      array[vFirst] = scanFile.nextDouble();
     }
     System.out.println("Please enter the elements of the 2nd vector: ");
     double[] array2 = new double[input];
     for (int vSecond = 0; vSecond < array2.length; vSecond++){
-      array2[vSecond] = input.nextDouble();
+      array2[vSecond] = scanFile.nextDouble();
     }
-      //The result will be calculated with a separate function that uses
+    //The result will be calculated with a separate function that uses
     //convolution.
-    double[] vResult = convolution(array, array2);
-    //Display the result.
+    double[] vResult = convolveVectors(array, array2);
     for (int vFirst = 0; vFirst < vResult.length; vFirst++){
       System.out.print((int) vResult[vFirst] + " ");
     }
   }
   //Create a separate function that will use the convolution function
-  public static double[] convolution(double[] vFirst, double[] vSecond) {
+  public static double[] convolveVectors(double[] vFirst, double[] vSecond) {
     //The vResult will be set here and and set equal to the sum of both vectors
     double[] vResult = new double[(vFirst.length + vSecond.length) - 1];
     //cResult will be used to shift the second vector until it hits the value of vResult.
     for (int cResult = 0; cResult < vResult.length; cResult++){
       vResult[cResult] = 0;
-      //Setup a for loop to keep track of how much the second vector shifts
-      for (int shift = 0; shift < 51; shift++){
-        double sum = 0;
-        //if the difference between cResult & shift = 0 & is less than the the vector's length
-        if (cResult - shift >= 0 && cResult - shift < vFirst.length){
-          //If it's less than the second vector's length
-          if (cResult >= 0 && cResult < vSecond.length)
-          //set the sum to what the first vector's difference multipled by
-          //The second vector
-            sum = (vFirst[cResult - shift] * vSecond[shift]);
+    //Setup a for loop to keep track of how much the second vector shifts
+    for (int shift = 0; shift < 51; shift++){
+      double sum = 0;
+      //if the difference between cResult & shift = 0 & is less than the the vector's length
+      if (cResult - shift >= 0 && cResult - shift < vFirst.length){
+        //If it's less than the second vector's length  
+        if (cResult >= 0 && cResult < vSecond.length)
+        //set the sum to what the first vector's difference multipled by
+        //The second vector
+        sum = (vFirst[cResult - shift] * vSecond[shift]);
         }
+        //Return the result to give the answer
         vResult[cResult] += sum;
       }
     }
-    //Return the result to give the answer
     return vResult; 
   }
 }
